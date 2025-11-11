97aed297063d75ea5e454a9a9cf3aa8c1e3db196
kkkathy211 <wyanxi211@gmail.com>
2025-11-10 20:50:11 -0500

    Completed Problem 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,41 +41,70 @@ st.write("## Age-specific cancer mortality rates")
 
 ### P2.1 ###
 # replace with st.slider
-year = 2012
+year = st.slider(
+    "Select Year",
+    min_value=int(df["Year"].min()),
+    max_value=int(df["Year"].max()),
+    value=int(df["Year"].min()),
+    step=1
+)
+
 subset = df[df["Year"] == year]
 ### P2.1 ###
 
+st.write(subset)
+
+import streamlit as st
 
 ### P2.2 ###
 # replace with st.radio
-sex = "M"
+sex = st.radio(
+    "Select Sex",
+    options=["M","F"],
+    index=0
+)
+
 subset = subset[subset["Sex"] == sex]
 ### P2.2 ###
 
+st.write(subset)
 
 ### P2.3 ###
 # replace with st.multiselect
 # (hint: can use current hard-coded values below as as `default` for selector)
-countries = [
-    "Austria",
-    "Germany",
-    "Iceland",
-    "Spain",
-    "Sweden",
-    "Thailand",
-    "Turkey",
-]
+all_countries = df["Country"].unique().tolist()
+countries = st.multiselect(
+    "Select Countries",
+    options=all_countries,
+    default=[
+        "Austria",
+        "Germany",
+        "Iceland",
+        "Spain",
+        "Sweden",
+        "Thailand",
+        "Turkey",
+    ]
+)
+
 subset = subset[subset["Country"].isin(countries)]
 ### P2.3 ###
 
+st.write(subset)
 
 ### P2.4 ###
 # replace with st.selectbox
-cancer = "Malignant neoplasm of stomach"
+cancer_type = subset["Cancer"].unique().tolist()
+
+cancer = st.selectbox(
+    "Select Cancer Type",
+    options = cancer_types,
+    index=cancer_type.index("Malignant neoplasm of stomach")
+)
 subset = subset[subset["Cancer"] == cancer]
 ### P2.4 ###
 
-
+st.write(subset)
 ### P2.5 ###
 ages = [
     "Age <5",
@@ -88,11 +117,30 @@ ages = [
     "Age >64",
 ]
 
-chart = alt.Chart(subset).mark_bar().encode(
+brush = alt.selection_interval(encodings=['x'])
+
+chart = alt.Chart(subset).mark_rect().encode(
+    x=alt.X("Age", sort=ages, title="Age Group"),
+    y=alt.Y("Country", title="Country"),
+    color=alt.Color(
+        "Rate",
+        scale=alt.Scale(type="log", domain=[0.01, 1000], clamp=True)
+        title="Mortality rate per 100k",
+    ),
+    tooltip=["Country","Age","Rate"],
+).add_params(
+    brush
+).properties(
+    title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
+)
+
+chart2 = alt.Chart(subset).mark_bar().encode(
     x=alt.X("Age", sort=ages),
     y=alt.Y("Rate", title="Mortality rate per 100k"),
     color="Country",
     tooltip=["Rate"],
+).transform_filter(
+    brush    
 ).properties(
     title=f"{cancer} mortality rates for {'males' if sex == 'M' else 'females'} in {year}",
 )
